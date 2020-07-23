43189f2b1da0e05c30636c1779153d34e44ae274
藤井祐和 <fu-hiz@MacBook-Pro.local>
2020-07-23 20:03:06 +0900

    formatter

--- pages/prism.vue
+++ pages/prism.vue
@@ -1,5 +1,4 @@
 <template>
-<div>
   <v-layout>
     <v-flex class="text-center">
       <pre data-file="wewew.js"><code class="language-css" data-file="aaa.js">
@@ -8,17 +7,17 @@ p { color: blue }
 p { color: green }</code>
 </pre>
 
-<pre><code class="language-css">p { color: blue }
+      <pre><code class="language-css">p { color: blue }
 p { color: blue }
+p { color: green }</code>
+</pre>
+
+      <pre><code class="language-css">p { color: blue }
+p { color: bluedwdwdwdwdwdwdbluedwdwdwdwdwdwdbluedwdwdwdwdwdwdbluedwdwd }
 p { color: green }</code>
 </pre>
     </v-flex>
   </v-layout>
-<pre><code class="language-css">p { color: blue }
-p { color: blue }
-p { color: green }</code>
-</pre>
-</div>
 </template>
 
 <script>
